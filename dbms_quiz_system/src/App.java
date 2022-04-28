1eb6c3e8fdf778a736cb2733bc8c051813281e70
iamangangwar <amangangwar084@gmail.com>
2022-04-28 12:23:28 +0530

    working code

--- dbms_quiz_system/src/App.java
+++ dbms_quiz_system/src/App.java
@@ -1,5 +1,45 @@
+import java.sql.*;
+import java.sql.Connection;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+    public static void main(String arg[])
+    {
+        Connection connection = null;
+        try {
+            // below two lines are used for connectivity.
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = (Connection) DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/quiz_system",
+                "root", "aman");
+ 
+            // quiz_system is database
+            // root is name of database
+            // aman is password of database
+ 
+            Statement statement;
+            statement = connection.createStatement();
+            ResultSet resultSet;
+            resultSet = statement.executeQuery(
+                "select * from student_name");
+            
+            String first;
+            String middle;
+            String last;
+
+            while (resultSet.next()) {
+                first = resultSet.getString("FirstName");
+                middle = resultSet.getString("MiddleName");
+                last = resultSet.getString("LastName");
+                System.out.println("First : " + first
+                                    + " Middle : " + middle
+                                    + " Last : " + last + "\n");
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        }
+        catch (Exception exception) {
+            System.out.println(exception);
+        }
+    } // function ends
+} // class ends
---
+++ dbms_quiz_system/src/Connection.java
@@ -0,0 +1,18 @@
+import java.sql.*;
+//import java.sql.DriverManager;
+
+public class Connection {
+	static Connection con;
+    public static Connection create_con(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/quiz_system",
+                "root", "aman");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
